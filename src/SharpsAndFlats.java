445b16f27f24f88241b6cc0bc2e9ae2762bbfdca
Mike <35826344+mjfernez@users.noreply.github.com>
2018-12-06 19:12:29 -0500

    Update SharpsAndFlats.java
    added comments

--- src/SharpsAndFlats.java
+++ src/SharpsAndFlats.java
@@ -1,3 +1,8 @@
+/*
+*Helper class that is effectively the circle of firths
+*for any note, returns info on the key
+*/
+
 public class SharpsAndFlats 
 {
     public static final String FLAT = "b";
@@ -5,6 +10,10 @@ public class SharpsAndFlats
     public static final String[] KEYSWITHFLATS = { "C", "F", "Bb", "Eb","Ab","Db", "Gb", "Cb" };
     public static final String[] KEYSWITHSHARPS = { "C", "G", "D", "A","E","B", "F#", "C#" };
     
+    /*
+    *Returns number of flats (or sharps) in a given key
+    *example, Key of G has one sharp (f#)
+    */
     public static int getNumAccidentals(String note)
     {
         for(int i = 0; i < KEYSWITHFLATS.length; i++)
@@ -16,6 +25,8 @@ public class SharpsAndFlats
                 return i;
         return -1;
     }
+    //Since sharps and flats are equivalent I chose to stick with flats
+    //this method returns true if the key has flats, false if it has sharps
     public static boolean isFlat(String note)
     {
         for(String s : KEYSWITHFLATS)
